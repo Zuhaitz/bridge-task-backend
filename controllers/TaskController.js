dad5d91570e487b0f5e21896117a84673a650f22
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-08-24 13:45:32 +0200

    more endpoints done

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -10,6 +10,37 @@ const TaskController = {
       res.status(400).send({ message: "Task could not be created" });
     }
   },
+  async getAll(req, res) {
+    try {
+      const tasks = await Task.find();
+      res.send(tasks);
+    } catch (error) {
+      console.error(error);
+      res.status(400).send({ message: "Tasks could not be retrieved" });
+    }
+  },
+  async getById(req, res) {
+    try {
+      const task = await Task.findById(req.params.id);
+      res.send(task);
+    } catch (error) {
+      console.error(error);
+      res.status(400).send({ message: "Could not find task by id" });
+    }
+  },
+  async completed(req, res) {
+    try {
+      const task = await Task.findByIdAndUpdate(
+        req.params.id,
+        { completed: true },
+        { new: true }
+      );
+      res.send(task);
+    } catch (error) {
+      console.error(error);
+      res.status(400).send({ message: "Could not update task" });
+    }
+  },
 };
 
 module.exports = TaskController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -4,5 +4,8 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/", TaskController.create);
+router.get("/", TaskController.getAll);
+router.get("/id/:id", TaskController.getById);
+router.put("/id/:id", TaskController.completed);
 
 module.exports = router;
